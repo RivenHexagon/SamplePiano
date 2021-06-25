8ab017c1a585537aa6932c9ca7f58ad94d5186b4
Kolja Funkmeyer <Kolja.Funkmeyer@gmx.eu>
2021-06-25 10:25:54 +0200

    listen to aseqdump and print line via python3

---
+++ PianoMIDI.py
@@ -0,0 +1,17 @@
+import subprocess
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+if __name__ == "__main__":
+    for path in execute(["aseqdump", "-p", "24"]):
+        print(path, end="")
+
+
+
