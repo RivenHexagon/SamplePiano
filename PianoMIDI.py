1bc5366e89ef6fd7b525a0eea5dd53ba6bf3fc05
Kolja Funkmeyer <Kolja.Funkmeyer@gmx.eu>
2021-06-29 22:00:48 +0200

    exit lowest key and function for play

--- PianoMIDI.py
+++ PianoMIDI.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 import AseqdumpParser as adp
 from playsound import playsound
 
@@ -11,15 +12,19 @@ def execute(cmd):
     if return_code:
         raise subprocess.CalledProcessError(return_code, cmd)
 
-def playCat(_params):
+def evalNote(_params):
     findRes = _params[0].find('Note on')
     if -1 != findRes:
         print( _params[1] )
         if " note 48" == _params[1]:
-            playsound('3-2-10027.mp3')
+            playSound('3-2-10027.mp3')
+        elif " note 36" == _params[1]:
+            sys.exit()
+        
             
+def playSound(_title):
+    playsound(_title)
 
-    
 
 if __name__ == "__main__":
     myParser = adp.AseqdumpParser()
@@ -27,7 +32,7 @@ if __name__ == "__main__":
     for path in lineDump:
         strip  = " ".join(path.split()) #path.replace(" ","")
         params = strip.split(",")
-        playCat(params)
+        evalNote(params)
 
 #pygame.mixer.init()
 #pygame.mixer.music.load("file.mp3")
