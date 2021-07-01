e26ba63275089fdfb7ab03f05219cbb059f49e28
Riven Hexagon <RivenHexagon@gmx.de>
2021-07-01 12:20:22 +0200

    major refactoring

---
+++ SamplePiano.py
@@ -0,0 +1,86 @@
+import subprocess
+import sys
+import threading
+#import AseqdumpParser as adp
+from playsound import playsound
+
+sampleTable = { "note 48": 'cat-moaning.mp3', 
+                "note 50": 'dog-barking.wav',}
+
+def executeAndYieldLinesFromStdout(_cmd):
+    popen = subprocess.Popen( _cmd, stdout=subprocess.PIPE,
+                              universal_newlines=True )
+
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+
+    popen.stdout.close()
+    return_code = popen.wait()
+
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, _cmd)
+
+
+#TODO make parser for those lines that gives full content
+def parseAseqdumpLine(_line):
+    singleBlanksLine = " ".join( _line.split() ) # remove consecutive blanks
+    lineSegments     = singleBlanksLine.split( "," )
+
+    if isNoteOn( lineSegments[0] ):
+        note = lineSegments[1][1:] # [1:] ommits leading blank
+        evalNote( note )
+
+
+def isNoteOn(_lineSegment):
+    findCount = _lineSegment.find( 'Note on' )
+
+    if -1 != findCount:
+        return True
+    else:
+        return False
+
+
+def evalNote(_note):
+    checkExitOnNote( _note )
+
+    try:
+        audioFileName = sampleTable[_note]
+        if noSoundIsPlaying():
+            print( "Playing sample for", _note )
+            makeNoise( audioFileName )
+    except:
+        print( "No sample for", _note )
+
+
+def checkExitOnNote(_note):
+    if "note 36" == _note:
+        print("sys.exit() on note 36")
+        sys.exit()
+
+
+def noSoundIsPlaying():
+    cnt = threading.active_count()
+
+    if cnt < 2:
+        return True
+    else:
+        return False
+
+
+def makeNoise(_title):
+        t1 = threading.Thread( target=playsound, args=(_title,) )
+        t1.start()
+
+
+if __name__ == "__main__":
+
+    #myParser = adp.AseqdumpParser()
+    aseqdumpLines = executeAndYieldLinesFromStdout(["aseqdump", "-p", "28"])
+
+    for line in aseqdumpLines:
+        parseAseqdumpLine( line )
+
+''' END '''
+
+#line.replace(" ","")
+        
