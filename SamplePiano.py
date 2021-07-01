22f11f9e2ea30d8e60e991545c0e58eb3296c9c8
Riven Hexagon <RivenHexagon@gmx.de>
2021-07-02 00:37:29 +0200

    cleanup

--- SamplePiano.py
+++ SamplePiano.py
@@ -20,15 +20,11 @@ sampleTable = { "note 48": 'cat-moaning.mp3',
                 "note 50": 'dog-barking.wav',}
 
 def executeAndYieldStdout(_cmd, _lineQ):
-    #global stop_flag
     popen = subprocess.Popen( _cmd, stdout=subprocess.PIPE,
                               universal_newlines=True )
 
     for stdout_line in iter(popen.stdout.readline, ""):
         _lineQ.put( stdout_line )
-        #if stop_flag:
-            #break
-        #yield stdout_line 
 
     popen.stdout.close()
     return_code = popen.wait()
@@ -69,9 +65,7 @@ def evalNote(_note):
 
 
 def checkExitOnNote(_note):
-    global stop_flag
     if "note 36" == _note:
-        #stop_flag = True
         print("sys.exit() on note 36")
         sys.exit()
 
@@ -100,19 +94,11 @@ if __name__ == "__main__":
                                  args=(aseqDumpArgs, lineQ) )
     aseqDump.daemon = True
     aseqDump.start()
+
     while True:
         line = lineQ.get()
-        #print( line )
         parseAseqdumpLine( line )
-        #if stop_flag:
-            #break
 
-    aseqDump.join()
-    #myParser = adp.AseqdumpParser()
-    #aseqdumpLines = executeAndYieldStdout(["aseqdump", "-p", "28"])
-
-    #for line in aseqdumpLines:
-        #parseAseqdumpLine( line )
 
 ''' END '''
 
